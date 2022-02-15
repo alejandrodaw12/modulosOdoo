870780448187cb13cbd06c69754922331781e133
alejandrodaw12 <59731511+alejandrodaw12@users.noreply.github.com>
2022-02-15 19:57:31 +0100

    error

--- __manifest__.py
+++ __manifest__.py
@@ -23,7 +23,7 @@
 
     # always loaded
     'data': [
-        'security/proyectos_security.xml',
+        #'security/proyectos_security.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
